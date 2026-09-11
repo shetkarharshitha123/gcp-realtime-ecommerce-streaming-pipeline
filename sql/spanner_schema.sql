2adc328bc76e16356f0b496e01f4c554b51ec355
Shetkar Harshitha Patil <shetkarharsha49@gmail.com>
2026-09-11 16:48:54 +0530

    Create EcomOrders table in spanner_schema.sql

--- sql/spanner_schema.sql
+++ sql/spanner_schema.sql
@@ -1,1 +1,7 @@
-
+CREATE TABLE EcomOrders (
+    order_id STRING(64) NOT NULL,
+    product_id STRING(64) NOT NULL,
+    user_id STRING(64),
+    quantity INT64,
+    order_date TIMESTAMP
+) PRIMARY KEY (order_id, product_id);
