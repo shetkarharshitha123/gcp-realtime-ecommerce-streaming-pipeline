4b987f8a3db6a2f6b8b0a2ddca23fe0444bc23e4
Shetkar Harshitha Patil <shetkarharsha49@gmail.com>
2026-09-11 16:50:00 +0530

    Add summary query for daily product analytics

--- sql/verification_queries.sql
+++ sql/verification_queries.sql
@@ -1,1 +1,7 @@
+SELECT  
+  COUNT(*) AS total_summary_rows, 
+  SUM(total_items_sold) AS total_units_sold, 
+  SUM(total_orders) AS total_orders_processed, 
+  MAX(aggregated_at) AS last_updated_at 
+FROM `snappy-mapper-498509e0.ecom_analytics.daily_product_summary 
 
